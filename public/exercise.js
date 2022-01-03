2b8ffe30c4be6719294312217d253acb0079d7d0
Steven Sosebee <swsosebee@gmail.com>
2022-01-03 11:00:26 -0500

    corrected API.js call

--- public/exercise.js
+++ public/exercise.js
@@ -13,7 +13,6 @@ const completeButton = document.querySelector("button.complete");
 const addButton = document.querySelector("button.add-another");
 const toast = document.querySelector("#toast");
 const newWorkout = document.querySelector(".new-workout");
-// import { createWorkout, addExercise } from "./api";
 
 let workoutType = null;
 
